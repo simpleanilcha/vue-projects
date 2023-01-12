e2dcabe87fc31c8f162216e26ed97710eb96d9c0
Anil Maharjan <simpleanilcha@gmail.com>
2023-01-12 12:31:31 +0545

    handling API errors

--- weather-app/src/views/HomeView.vue
+++ weather-app/src/views/HomeView.vue
@@ -5,9 +5,14 @@
         class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]">
       <ul v-if="mapboxSearchResults"
         class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]">
-        <li v-for="searchResult in mapboxSearchResults" :key="searchResult.id" class="py-2 cursor-pointer">
-          {{ searchResult.place_name }}
-        </li>
+        <p v-if="searchError">Sorry, something went wrong, please try again.</p>
+        <p v-if="!searchError && mapboxSearchResults.length === 0">No results match your query, try a different term.
+        </p>
+        <template v-else>
+          <li v-for="searchResult in mapboxSearchResults" :key="searchResult.id" class="py-2 cursor-pointer">
+            {{ searchResult.place_name }}
+          </li>
+        </template>
       </ul>
     </div>
   </main>
@@ -21,14 +26,19 @@ const searchQuery = ref('');
 const queryTimeout = ref(null);
 const mapboxAPIKey = 'pk.eyJ1IjoiYW5pbGNoYSIsImEiOiJjbDdjYzhieXYwZ2Q2M29xYzI2eDdrMGYzIn0.EyTc0gmFBI_iwfLnHrCXjw'
 const mapboxSearchResults = ref(null);
+const searchError = ref(null);
 
 const getSearchResults = () => {
   clearTimeout(queryTimeout.value);
   queryTimeout.value = setTimeout(async () => {
     if (searchQuery.value !== '') {
-      const result = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}&types=place`)
-      mapboxSearchResults.value = result.data.features;
-      // console.log(mapboxSearchResults.value)
+      try {
+        const result = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}&types=place`)
+        mapboxSearchResults.value = result.data.features;
+      } catch (error) {
+        searchError.value = true
+      }
+
       return;
     }
     mapboxSearchResults.value = null
