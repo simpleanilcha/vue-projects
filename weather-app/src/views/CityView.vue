e5981748df8bff973f3a4fb493a72390250e5f58
Anil Maharjan <simpleanilcha@gmail.com>
2023-01-12 15:33:13 +0545

    add: city view layout

--- weather-app/src/views/CityView.vue
+++ weather-app/src/views/CityView.vue
@@ -1,9 +1,15 @@
 <template>
   <div>
-
+    <Suspense>
+      <AsyncCityView />
+      <template #fallback>
+        <p class="text-white text-center py-10">Loading...</p>
+      </template>
+    </Suspense>
   </div>
 </template>
 
 <script setup>
+import AsyncCityView from '../components/AsyncCityView.vue';
 
 </script>
